95ad7554d528d98b520cde6329bdd5a8bd6f19cb
aleksandr.bolshakov <aleksandr.bolshakov@synesis.ru>
2020-07-23 15:59:05 +0300

    update strings parsing and document

--- src/main/kotlin/exporter/generator/strings/StringGenerator.kt
+++ src/main/kotlin/exporter/generator/strings/StringGenerator.kt
@@ -20,7 +20,6 @@ import java.io.BufferedWriter
 
 
 class StringGenerator {
-    private val APPLICATION_NAME = "Google Sheets API Java Quickstart"
     private val JSON_FACTORY = JacksonFactory.getDefaultInstance()
     private val TOKENS_DIRECTORY_PATH = "tokens"
 
@@ -57,8 +56,8 @@ class StringGenerator {
 
     fun generate(e: AnActionEvent): String {
         val HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport()
-        val spreadsheetId = "154-T6SHC9Cjs-va3DUDL2D56pmbsyjhBtevsAmpPeQA";
-        val range = "A1:R";
+        val spreadsheetId = "12iDxfLlg_wVYQLwLj_NI33DJNjzImEO-uU3NSsCZGCg";
+        val range = "A1:U";
         val service = Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
             .setApplicationName(APPLICATION_NAME)
             .build()
@@ -69,25 +68,35 @@ class StringGenerator {
 
         val values = response.getValues()
 
-        val languages = values.first().filter { it.toString().isNotEmpty() }
-        val strings = languages.mapIndexed { index, languagePostfix ->
-            Strings(
-                languagePostfix.toString(),
-                values.takeLast(values.size - 1).mapNotNull { rows ->
-                    try {
-                        if (rows[0].toString().isBlank()) {
-                            return@mapNotNull null
+        var languages = values.first().filter { (it as String).isNotBlank() }
+        languages = languages.takeLast(languages.size - 4)
+
+        val strings = languages.mapIndexedNotNull { index, language ->
+            try {
+                Strings(
+                    translation = "-"+language as String,
+                    values = values.takeLast(values.size - 1).mapNotNull { row ->
+                        try {
+                            val key = row[0].toString()
+                            if (key.isNotBlank()) {
+                                val value = fixParams(row[index + 4].toString())
+                                Translation(
+                                    key = key,
+                                    value = value
+                                )
+                            } else {
+                                null
+                            }
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            null
                         }
-                        Translation(
-                            rows[0].toString(),
-                            fixParams(rows[index + 1].toString())
-                        )
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                        println(rows)
-                        null
                     }
-                })
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
         }
         generateFiles(e.project!!.basePath!!, strings)
         return values.toTypedArray().contentToString()
@@ -143,4 +152,8 @@ class StringGenerator {
 
     private data class Strings(val translation: String, val values: List<Translation>)
     private data class Translation(val key: String, val value: String)
+
+    companion object {
+        private const val APPLICATION_NAME = "GEM4ME String Generator"
+    }
 }
